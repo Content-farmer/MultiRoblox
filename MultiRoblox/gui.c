619956b4ee450d850920ff180eb62a49e500b089
Content-farmer <contentfarmer23@gmail.com>
2026-05-18 12:40:18 -0600

    Fix Roblox multi-instance locks

--- MultiRoblox/gui.c
+++ MultiRoblox/gui.c
@@ -1,45 +1,93 @@
 #include <windows.h>
+#include <stdio.h>
 
 #include "gui.h"
+
+static BOOL LockIsActive(HANDLE handle, DWORD error)
+{
+    return handle != NULL && error != ERROR_ALREADY_EXISTS;
+}
+
+static void SetStatusText(void)
+{
+    WCHAR status[512];
+    BOOL mutexActive = LockIsActive(g_singleton_mutex, g_singleton_mutex_error);
+    BOOL eventActive = LockIsActive(g_singleton_event, g_singleton_event_error);
+
+    if (mutexActive || eventActive)
+    {
+        swprintf_s(status, 512,
+            L"Status: ready. Keep this window open while launching Roblox.\r\n"
+            L"Legacy lock: %s\r\n"
+            L"Current lock: %s",
+            mutexActive ? L"active" : L"not created",
+            eventActive ? L"active" : L"not created");
+    }
+    else
+    {
+        swprintf_s(status, 512,
+            L"Status: Roblox may already be running. Close every Roblox window, "
+            L"restart MultiRoblox, then launch Roblox again.");
+    }
+
+    SetWindowTextW(g_status, status);
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
     case WM_DESTROY:
+        StopRobloxLocks();
         PostQuitMessage(0);
         return 0;
 
     default:
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        return DefWindowProcW(hwnd, uMsg, wParam, lParam);
     }
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    WNDCLASS wc = { 0 };
+    StartRobloxLocks();
+
+    WNDCLASSW wc = { 0 };
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = "MULTIROBLOX_CLASS";
+    wc.lpszClassName = L"MULTIROBLOX_CLASS";
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 
-    RegisterClass(&wc);
+    RegisterClassW(&wc);
 
-    g_hwnd = CreateWindowEx(0, "MULTIROBLOX_CLASS", L"MultiRoblox", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 400, 455, NULL, NULL, hInstance, NULL);
-    g_title = CreateWindow(L"STATIC", L"MultiRoblox 0.0.3", WS_VISIBLE | WS_CHILD, 10, 10, 365, 20, g_hwnd, NULL, hInstance, NULL);
-    g_textbox = CreateWindow(L"EDIT", L"Thanks for using MultiRoblox!\r\n\r\nIn case if you're wondering, you must open\r\nMultiRoblox FIRST before opening multiple ROBLOX\r\nsessions. Once you close MultiRoblox, all but one\r\nROBLOX instance will close.\r\n\r\nDiscord: https://discord.gg/g2Jgw8Z6Rb\r\nWeAreDevs: https://forum.wearedevs.net/t/29695\r\nGitHub: https://github.com/Dashbloxx/MultiRoblox\r\n", WS_BORDER | WS_VISIBLE | WS_CHILD | ES_LEFT | ES_AUTOHSCROLL | ES_MULTILINE | ES_READONLY, 10, 40, 365, 365, g_hwnd, NULL, hInstance, NULL);
+    g_hwnd = CreateWindowExW(0, L"MULTIROBLOX_CLASS", L"MultiRoblox", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 430, 360, NULL, NULL, hInstance, NULL);
     if (g_hwnd == NULL)
     {
         return 0;
     }
 
+    g_title = CreateWindowW(L"STATIC", L"MultiRoblox 0.0.4", WS_VISIBLE | WS_CHILD, 14, 14, 390, 22, g_hwnd, NULL, hInstance, NULL);
+    g_status = CreateWindowW(L"STATIC", L"", WS_VISIBLE | WS_CHILD, 14, 44, 390, 64, g_hwnd, NULL, hInstance, NULL);
+    g_textbox = CreateWindowW(L"EDIT",
+        L"How to use:\r\n\r\n"
+        L"1. Close every Roblox window.\r\n"
+        L"2. Open MultiRoblox and leave this window open.\r\n"
+        L"3. Launch Roblox on each account you want to use.\r\n\r\n"
+        L"Closing MultiRoblox releases the lock, so Roblox may return to one instance.",
+        WS_BORDER | WS_VISIBLE | WS_CHILD | ES_LEFT | ES_MULTILINE | ES_READONLY | WS_VSCROLL,
+        14, 120, 390, 180, g_hwnd, NULL, hInstance, NULL);
+
+    SetStatusText();
+
     ShowWindow(g_hwnd, nCmdShow);
+    UpdateWindow(g_hwnd);
 
     MSG msg = { 0 };
-    while (GetMessage(&msg, NULL, 0, 0))
+    while (GetMessageW(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        DispatchMessageW(&msg);
     }
 
-    return msg.wParam;
+    return (int)msg.wParam;
 }
